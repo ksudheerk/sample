474c9aea473998b5e00d62c2c377ec9b070af80d
sudheer.c <sudheer.c@YH1004569LT.Yash.com>
2018-03-09 21:00:33 +0530

    first commit

---
+++ unit-tests/src/test/java/com/yash/javaapp1/sample/UnitTest.java
@@ -0,0 +1,12 @@
+package com.yash.javaapp1.sample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class UnitTest {
+    @Test
+    public void test() {
+        assertTrue(true);
+    }
+}
